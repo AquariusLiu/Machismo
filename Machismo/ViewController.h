65eebc429a4f8626cc1d09a0f5dc9c2160e45a81
Liu XiangYu <liuxiangyu@LiudeMacBook-Air.local>
2015-01-18 11:25:53 +0800

    Initial Commit

---
+++ Machismo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Machismo
+//
+//  Created by Liu XiangYu on 15/1/18.
+//  Copyright (c) 2015年 Liu XiangYu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Machismo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Machismo
+//
+//  Created by Liu XiangYu on 15/1/18.
+//  Copyright (c) 2015年 Liu XiangYu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
